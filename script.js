daec516ef14b8274379222e7b46bd1854d4f01eb
Marta Sandoval Vinet <83969423+MartaSVinet@users.noreply.github.com>
2023-05-06 10:44:25 -0400

    Improve of functions
    Improving the round

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function computerPlay(computerChoice) {
 
 //The player makes a choice
 
-playerSelection = prompt("Choose your weapon! Type rock, paper or scissors.").toLowerCase();
+let playerSelection = prompt("Choose your weapon! Type rock, paper or scissors.").toLowerCase();
 const computerSelection = computerPlay();
 
 //We store every win
@@ -28,19 +28,23 @@ function playRound (playerSelection, computerSelection) {
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")
     ){
-        return ("You Win! " + playerSelection + " beats " + computerSelection) && playerWins++; 
-    } else if (playerSelection === computerSelection){
-        return ("You're even! " + playerSelection + " is equal to " + computerSelection); 
-    } else {
-        return ("You Lose! " + computerSelection + " beats " + playerSelection) && computerWins++;
-    }    
+        playerWins++; 
+    } else if ((computerSelection === "rock" && playerSelection === "scissors") || 
+                (computerSelection === "scissors" && playerSelection === "paper") ||
+                (computerSelection === "paper" && playerSelection === "rock")
+    ){
+        computerWins++;         
+    } else if (computerSelection === playerSelection) {
+        return ("You're even! " + playerSelection + " is equal to " + computerSelection);
+    }
 }
+console.log(playerSelection, computerSelection);
 
 //Five rounds
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        console.log(playRound());
+        console.log(playRound(), i);
     }
     if (playerWins > computerWins) {
         console.log("OMG you just beat the Skynet! Congratulations, winner");
@@ -50,7 +54,4 @@ function game() {
         console.log("Draw!");
     }    
 }
-
-console.log(game())
-
-
+game(playerSelection,computerSelection)
