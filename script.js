4a3871d1a0e47d29de541591407611d57a4b2606
Marta Sandoval Vinet <83969423+MartaSVinet@users.noreply.github.com>
2023-05-09 16:04:27 -0400

    Separating functions

--- script.js
+++ script.js
@@ -1,57 +1,69 @@
-//The computer chooses randomly
-function computerPlay(computerChoice) {     
-    let randomPc = (Math.floor(Math.random() *3));    
-    if (randomPc === 0) {
-        computerChoice ="paper";
-    } else if (randomPc === 1) {
-        computerChoice ="scissors";
-    } else {
-        computerChoice="rock";
-    } 
-    return computerChoice;     
-}
-
-//The player makes a choice
-
-let playerSelection = prompt("Choose your weapon! Type rock, paper or scissors.").toLowerCase();
-const computerSelection = computerPlay();
-
-//We store every win
-
-let playerWins = 0;
-let computerWins = 0;
-
-//The player v/s the computer
-
-function playRound (playerSelection, computerSelection) {
-    if ((playerSelection === "rock" && computerSelection === "scissors") || 
-        (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "rock")
-    ){
-        playerWins++; 
-    } else if ((computerSelection === "rock" && playerSelection === "scissors") || 
-                (computerSelection === "scissors" && playerSelection === "paper") ||
-                (computerSelection === "paper" && playerSelection === "rock")
-    ){
-        computerWins++;         
-    } else if (computerSelection === playerSelection) {
-        return ("You're even! " + playerSelection + " is equal to " + computerSelection);
+function firstSteps() {
+    alert("So you have decided to go against the Skynet. Ok, I'll give you the chance to settle this like adults...");
+    if( alert = true){
+        let playerSelection = prompt("Choose your weapon! Type rock, paper or scissors.").toLowerCase().trim();
+        return playerSelection;
     }
-}
-console.log(playerSelection, computerSelection);
-
-//Five rounds
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(), i);
-    }
-    if (playerWins > computerWins) {
-        console.log("OMG you just beat the Skynet! Congratulations, winner");
-    } else if (computerWins > playerWins) {
-        console.log("I'm sorry, technology beat you up! You lose");
+    else if (playerSelection === null) {
+        return askPlayer        
     } else {
-        console.log("Draw!");
-    }    
+        return computerPlay        
+    }
+} 
+
+let askPlayer = confirm("You have pressed the cancel button. Do you want to leave or get back to the game?")
+
+if(confirm) {
+    alert("See you soon")    
 }
-game(playerSelection,computerSelection)
+else {
+    return playerSelection;    
+}
+
+
+function computerPlay() {    
+    const plays = ["Rock", "Paper", "Scissors"];
+    return plays[Math.floor(Math.random() * plays.length)];
+  }
+  
+  function playRound(playerSelection, computerSelection) {    .
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if (
+      (playerSelection === "Rock" && computerSelection === "Scissors") ||
+      (playerSelection === "Paper" && computerSelection === "Rock") ||
+      (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+      return `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+      return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+  }
+  
+  function game() {    
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 0; i < 5; i++) {     
+      const playerSelection = prompt("Rock, paper, or scissors?");
+      const computerSelection = computerPlay();
+      console.log(playRound(playerSelection, computerSelection));
+      
+      const result = playRound(playerSelection, computerSelection);
+      if (result.includes("win")) {
+        playerScore++;
+      } else if (result.includes("lose")) {
+        computerScore++;
+      }
+    }
+  
+    if (playerScore > computerScore) {
+      console.log(`You win the game! Final score: ${playerScore} to ${computerScore}.`);
+    } else if (playerScore < computerScore) {
+      console.log(`You lose the game. Final score: ${computerScore} to ${playerScore}.`);
+    } else {
+      console.log(`It's a tie game. Final score: ${playerScore} to ${computerScore}.`);
+    }
+  }
+  
+  game();
