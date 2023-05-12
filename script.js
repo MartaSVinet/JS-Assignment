1db27c21fb374cb99e4ccb823daf011f4bcb2a3b
Marta Sandoval Vinet <83969423+MartaSVinet@users.noreply.github.com>
2023-05-11 20:36:10 -0400

    Better functions

--- script.js
+++ script.js
@@ -1,66 +1,55 @@
-function firstSteps() {
-    alert("So you have decided to go against the Skynet. Ok, I'll give you the chance to settle this like adults...");
-    if( alert = true){
-        let playerSelection = prompt("Choose your weapon! Type rock, paper or scissors.").toLowerCase().trim();
-        return playerSelection;
-    }
-    else if (playerSelection === null) {
-        return askPlayer        
-    } else {
-        return computerPlay()        
-    }
-} 
-firstSteps();
+let playerScore = 0;
+let computerScore = 0;
 
-function whatIf() {
-    let askPlayer = confirm("You have pressed the cancel button. Do you want to leave or get back to the game?")
-    if(confirm) {
-        alert("See you soon")    
-    }
-    else {
-        return playerSelection;    
-    }
+function greeting() {
+    alert("So you have decided to go against the Skynet. Ok, I'll give you the chance to settle this like adults...");
+} 
+
+function askUserToMakeChoice(){
+    let validSelection = false, playerSelection;    
+    while (!validSelection) {
+        playerSelection = prompt("Please enter your choice (rock, paper or scissors): ");       
+        if (playerSelection === null || playerSelection.trim() === '') {
+          alert("Invalid choice. Please try again."); 
+          continue;
+        }        
+        playerSelection = playerSelection.trim().toLowerCase();     
+        if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+          alert("Invalid choice. Please try again.");
+        } else {
+          validSelection = true;
+        }
+      }
+    return playerSelection; 
 }
 
-
-
-
 function computerPlay() {    
-    const plays = ["Rock", "Paper", "Scissors"];
-    return plays[Math.floor(Math.random() * plays.length)];
-  }
+    const plays = ["rock", "paper", "scissors"];
+    const computerSelection = plays[Math.floor(Math.random() * plays.length)]
+    return computerSelection;
+}
   
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
       return "It's a tie!";
-    } else if (
-      (playerSelection === "Rock" && computerSelection === "Scissors") ||
-      (playerSelection === "Paper" && computerSelection === "Rock") ||
-      (playerSelection === "Scissors" && computerSelection === "Paper")
-    ) {
+  } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+      playerScore += 1;
       return `You win! ${playerSelection} beats ${computerSelection}.`;
-    } else {
+  } else {
+      computerScore += 1;
       return `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }
   }
+}
   
-  function game() {    
-    let playerScore = 0;
-    let computerScore = 0;
-  
-    for (let i = 0; i < 5; i++) {     
-      const playerSelection = prompt("Rock, paper, or scissors?");
-      const computerSelection = computerPlay();
-      console.log(playRound(playerSelection, computerSelection));
-      
-      const result = playRound(playerSelection, computerSelection);
-      if (result.includes("win")) {
-        playerScore++;
-      } else if (result.includes("lose")) {
-        computerScore++;
-      }
+function game() {    
+    greeting();
+    for (let i = 0; i < 5; i++) {    
+      console.log(playRound(askUserToMakeChoice(), computerPlay())); 
     }
-  
     if (playerScore > computerScore) {
       console.log(`You win the game! Final score: ${playerScore} to ${computerScore}.`);
     } else if (playerScore < computerScore) {
@@ -70,4 +59,3 @@ function computerPlay() {
     }
   }
   
-  game();
